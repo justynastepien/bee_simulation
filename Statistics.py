6c025505b1f7d044f8cd4810a759cb1235b22c9a
justynastepien <jstepie@student.agh.edu.pl>
2023-12-03 20:05:35 +0100

    statistics and plots

---
+++ Statistics.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+
+
+class Statistic:
+    def __init__(self, start_time: time, board):
+        self.start_time = start_time
+        self.num_of_flowers = 20
+        self.times = [0]
+        self.avg = []
+        self.count_avg_distance(board)
+
+    def count_avg_distance(self, board):
+        count = 0
+        sum_distances = 0
+        hive = np.array((25, 25))
+        for x in range(board.shape[0]):
+            for y in range(board.shape[1]):
+                if board[x][y] >= 1000:
+                    b = np.array((x, y))
+                    sum_distances += np.linalg.norm(hive - b)
+                    count += 1
+
+        self.avg.append(sum_distances/count)
+
+    def draw_plot(self, time_flower, board):
+        self.times.append(time_flower - self.start_time)
+        self.num_of_flowers = self.num_of_flowers - 1
+
+        list_number = list(range(self.num_of_flowers, 21))
+        list_number.reverse()
+        k = 20 - self.num_of_flowers
+        plt.plot(self.times, list_number, 'ro')
+        plt.xlabel('time')
+        plt.ylabel('number of flowers')
+        plt.savefig('flowers_per_time/plot_' + str(k))
+        plt.show()
+
+        plt.plot(self.times, list_number)
+        plt.xlabel('time')
+        plt.ylabel('number of flowers')
+        plt.show()
+
+        self.count_avg_distance(board)
+        plt.plot(self.times, self.avg)
+        plt.xlabel('time')
+        plt.ylabel('average of distances flowers from hive')
+        plt.savefig('avg_per_time/plot_' + str(k))
+        plt.show()
+
+
+
+
